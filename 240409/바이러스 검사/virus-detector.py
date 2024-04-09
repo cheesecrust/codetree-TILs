8f6a51fc88f5de9a20971ed0e610c47fdb268d14
JUNG YEON SU <52970725+cheesecrust@users.noreply.github.com>
2024-04-09 20:33:16 +0900

    Title: 바이러스 검사, Time: 97ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240409/바이러스 검사/virus-detector.py
+++ 240409/바이러스 검사/virus-detector.py
@@ -7,10 +7,10 @@ master, member = map(int, input().split())
 answer = 0
 for customer in customers:
 
-    if(customer <= master):
+    if(customer < master):
         answer += 1
     else:
         customer -= master
-        answer += (1 + customer % member) 
+        answer += (1 + customer // member + (customer % member)) 
 
 print(answer)
