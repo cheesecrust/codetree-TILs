6d9d7b436578f97e8e7ae702948386adac3ccfd3
JUNG YEON SU <52970725+cheesecrust@users.noreply.github.com>
2024-04-10 17:43:59 +0900

    Title: 정육면체 굴리기, Time: 130ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240410/정육면체 굴리기/cube-rounding.py
+++ 240410/정육면체 굴리기/cube-rounding.py
@@ -1,18 +1,15 @@
-numMap = []
-n, m = 0, 0
-
 # 1 - 동, 2- 서, 3 - 북, 4 - 남
 
 def rolling (dice, y, x, direction):
     dy = [0, 0, 0, -1, 1]
     dx = [0, 1, -1, 0, 0]
 
-    top, bottom, front, back, left, right = dice
-
     ny, nx = y + dy[direction], x + dx[direction]
 
     if n <= ny or m <= nx or ny < 0 or nx < 0:
         return y, x, dice
+
+    top, bottom, front, back, left, right = dice
 
     # 동
     if direction == 1:
@@ -44,7 +41,6 @@ def rolling (dice, y, x, direction):
     #남
     elif direction == 4:
         print(back)
-
         if numMap[ny][nx] == 0:
             numMap[ny][nx] = front
             dice = (back, front, top, bottom, left, right)
@@ -55,7 +51,7 @@ def rolling (dice, y, x, direction):
     return (ny, nx, dice)
     
 if __name__ == "__main__":
-    n, m, x, y, k = map(int, input().split())
+    n, m, y, x, k = map(int, input().split())
     numMap = [ list(map(int, input().split())) for _ in range(n)]
     roll = list(map(int, input().split()))
 
