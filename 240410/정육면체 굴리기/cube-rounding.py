acda410447cc8c810134b7c1e39704ca6f5a8cad
JUNG YEON SU <52970725+cheesecrust@users.noreply.github.com>
2024-04-10 17:34:59 +0900

    Title: 정육면체 굴리기, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240410/정육면체 굴리기/cube-rounding.py
+++ 240410/정육면체 굴리기/cube-rounding.py
@@ -17,19 +17,40 @@ def rolling (dice, y, x, direction):
     # 동
     if direction == 1:
         print(left)
-        dice = (left, numMap[ny][nx], front, back, bottom, top)
+        if numMap[ny][nx] == 0:
+            numMap[ny][nx] = right
+            dice = (left, right, front, back, bottom, top)
+        else:
+            dice = (left, numMap[ny][nx], front, back, bottom, top)
+            numMap[ny][nx] = 0
     #서 
     elif direction == 2:
         print(right)
-        dice = (right, numMap[ny][nx], front, back, top, bottom)
+        if numMap[ny][nx] == 0:
+            numMap[ny][nx] = left
+            dice = (right, left, front, back, bottom, top)
+        else:
+            dice = (right, numMap[ny][nx], front, back, top, bottom)
+            numMap[ny][nx] = 0
     #북
     elif direction == 3:
         print(front)
-        dice = (front, numMap[ny][nx], bottom, top, left, right)
+        if numMap[ny][nx] == 0:
+            numMap[ny][nx] = back
+            dice = (front, back, bottom, top, left, right)
+        else:
+            dice = (front, numMap[ny][nx], bottom, top, left, right)
+            numMap[ny][nx] = 0
     #남
     elif direction == 4:
         print(back)
-        dice = (back, numMap[ny][nx], top, bottom, left, right)
+
+        if numMap[ny][nx] == 0:
+            numMap[ny][nx] = front
+            dice = (back, front, top, bottom, left, right)
+        else:
+            dice = (back, numMap[ny][nx], top, bottom, left, right)
+            numMap[ny][nx] = 0
 
     return (ny, nx, dice)
     
