17efefd5ab6a21792b2dc4959ae81d5e47be5e5a
JUNG YEON SU <52970725+cheesecrust@users.noreply.github.com>
2024-10-01 12:03:19 +0900

    Title: 외주 수익 최대화하기, Time: 123ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241001/외주 수익 최대화하기/max-of-outsourcing-profit.py
@@ -0,0 +1,24 @@
+chart = [list() for _ in range(15)]
+N = 0
+answer = 0
+
+def dfs(start, pay):
+    global answer
+
+    if start == N:
+        answer = max(answer, pay)
+        return
+    
+    for i in range(start, N):
+        dfs(start + (i - start) + chart[i][0], pay + chart[i][1])
+
+    return
+
+if __name__ == "__main__":
+    N = int(input())
+
+    for i in range(N):
+        chart[i] = list(map(int, input().split()))
+
+    dfs(0, 0)
+    print(answer)
